cd913418c2e19ba12ec1e0299a97204ba45a0ee8
acampb46 <acampb46@emich.edu>
2024-10-24 11:50:26 -0400

    added additional headers to axios calls

--- node-app/routes/start.js
+++ node-app/routes/start.js
@@ -174,7 +174,10 @@ const fetchHtmlWithAxios = async (url) => {
     try {
         const response = await axios.get(url, {
             headers: {
-                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
+                'Accept-Language': 'en-US,en;q=0.9',
+                'Accept-Encoding': 'gzip, deflate, br',
+                'Connection': 'keep-alive',
             },
         });
         return response.data; // The HTML content

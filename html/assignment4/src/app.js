598d46c29b5991994b41bbd19cdd52fbde2ec919
acampb46 <acampb46@emich.edu>
2024-11-19 10:57:30 -0500

    added to .gitignore files

--- html/assignment4/src/app.js
+++ html/assignment4/src/app.js
@@ -2,15 +2,12 @@ const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
 const session = require('express-session');
-const socketIo = require('socket.io');
 const http = require('http');
 const { notFound, errorHandler } = require('./middlewares/errorHandler');
-const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 require('dotenv').config();
 
 const app = express();
 const server = http.createServer(app);
-const io = require('./socket')(server); // Socket.io setup
 
 // Middleware
 app.use(bodyParser.urlencoded({ extended: true }));

4601d04b6390c78eda0a8c66a317250292107e12
acampb46 <acampb46@emich.edu>
2024-11-19 11:14:07 -0500

    added dotenv config to notifciationUtils

--- html/assignment4/src/utils/notificationUtils.js
+++ html/assignment4/src/utils/notificationUtils.js
@@ -1,5 +1,6 @@
 const nodemailer = require('nodemailer');
 const twilio = require('twilio');
+require('dotenv').config();
 
 const emailTransporter = nodemailer.createTransport({
     service: 'Gmail',

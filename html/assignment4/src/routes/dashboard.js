dc55715cb340408e07cae01fd2ea212192a12c5c
Ashlee Gerard <43120575+acampb46@users.noreply.github.com>
2024-11-20 16:29:08 -0500

    Update dashboard.js

--- html/assignment4/src/routes/dashboard.js
+++ html/assignment4/src/routes/dashboard.js
@@ -2,18 +2,6 @@ const express = require('express');
 const router = express.Router();
 const dashboardController = require('../controllers/dashboardController');
 
-app.get('/dashboard', (req, res) => {
-    const userItems = getUserItems(); // Fetch user's items
-    const purchasedItems = getPurchasedItems(); // Fetch purchased items
-    console.log("Rendering Dashboard.ejs");
-    res.render('dashboard', {
-        pageTitle: 'Your Dashboard',
-        headerText: 'Welcome to Your Dashboard',
-        userItems,
-        purchasedItems
-    });
-});
-
 router.get('/items', dashboardController.userItems);
 router.get('/purchases', dashboardController.purchasedItems);
 

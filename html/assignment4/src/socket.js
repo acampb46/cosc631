3418b8616dcf8e7562cf15465d791c9449296622
acampb46 <acampb46@emich.edu>
2024-11-22 15:09:48 -0500

    testing sockets

--- html/assignment4/src/socket.js
+++ html/assignment4/src/socket.js
@@ -1,7 +1,7 @@
 const socketIo = require('socket.io');
 
 module.exports = (server) => {
-    const io = socketIo(server);
+    const io = new socketIo(server);
 
     io.on('connection', (socket) => {
         console.log(`New client connected: ${socket.id}`);

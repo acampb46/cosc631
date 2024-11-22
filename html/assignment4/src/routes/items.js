daf19f3bc4b2e79774037700a45a9f773595648d
acampb46 <acampb46@emich.edu>
2024-11-21 23:32:44 -0500

    update image capture to just use url

--- html/assignment4/src/routes/items.js
+++ html/assignment4/src/routes/items.js
@@ -2,10 +2,6 @@ const express = require('express');
 const router = express.Router();
 const itemController = require('../controllers/itemController');
 
-// Middleware to handle file uploads
-const multer = require('multer');
-const upload = multer({ dest: 'public/images/' });
-
 // Serve the register page
 router.get('/submit', (req, res) => {
     res.render('submit-item', {

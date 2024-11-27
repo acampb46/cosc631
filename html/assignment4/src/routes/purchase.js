42ab4966e950594a60bcf92f0270e5ddb52354b0
acampb46 <acampb46@emich.edu>
2024-11-26 21:31:20 -0500

    testing buyNow functionality

--- html/assignment4/src/routes/purchase.js
+++ html/assignment4/src/routes/purchase.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 const purchaseController = require('../controllers/purchaseController');
 
-router.post('/buy', purchaseController.buyNow);
+router.post('/buyNow', purchaseController.buyNow);
 
 module.exports = router;

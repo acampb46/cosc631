dd1161b63ec60d6c901fa433051ef6786373c596
acampb46 <acampb46@emich.edu>
2024-11-29 10:19:51 -0500

    have buyNow return transactionId

--- html/assignment4/src/controllers/purchaseController.js
+++ html/assignment4/src/controllers/purchaseController.js
@@ -25,21 +25,9 @@ const purchaseController = {
             const transactionResponse = await createTransaction(userId, item.seller_id, itemId, totalPrice);
             const {transactionId} = transactionResponse;
 
-            console.log({
-                buyerId: userId,
-                sellerId: item.seller_id,
-                itemId,
-                amount: totalPrice,
-                transactionId,
-            });
+            // Step 4: Return the transactionId in the response
+            return res.status(200).json({ transactionId });
 
-            console.log("Loading Payment Creation...");
-            // Step 4: Send POST request to /payment/create with transactionId
-            const paymentCreationResponse = await axios.post(
-                `https://gerardcosc631.com/assignment4/payment/create`,
-                {transactionId},
-                {headers: {'Content-Type': 'application/json'}}
-            );
         } catch (error) {
             console.error('Error during purchase:', error);
             next(error);

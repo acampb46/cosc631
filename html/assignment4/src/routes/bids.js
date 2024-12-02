0ab0d64f250351f30cc2bc516cc220eb6f7ba041
acampb46 <acampb46@emich.edu>
2024-12-02 16:33:22 -0500

    start removing bid functionality

--- html/assignment4/src/routes/bids.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const bidController = require('../controllers/bidController');
-
-router.post('/place', bidController.placeBid);
-router.get('/highest/:itemId', bidController.getHighestBid);
-
-module.exports = router;

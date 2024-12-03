f1bff2f085b8b782e9faa1f819c6dfc88947c05d
acampb46 <acampb46@emich.edu>
2024-12-03 08:44:19 -0500

    debug where null is coming from

--- html/assignment4/src/controllers/itemController.js
+++ html/assignment4/src/controllers/itemController.js
@@ -46,6 +46,7 @@ const itemController = {
     }, async updateQuantity(req, res, next) {
         try {
             const {id, quantity} = req.body;
+            console.log('id: ' + id + 'quantity: ' + quantity);
             await Item.updateQuantity(id, quantity);
 
             // Get io from the app object
